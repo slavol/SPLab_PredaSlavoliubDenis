dca45a2d68132cbfacebbec8320a913d11ba7a5c
slavol <slavoliub.preda01@e-uvt.ro>
2025-11-07 09:22:11 +0200

    First commit

---
+++ src/main/java/com/example/splab_predaslavoliubdenis/SpLabPredaSlavoliubDenisApplication.java
@@ -0,0 +1,13 @@
+package com.example.splab_predaslavoliubdenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLabPredaSlavoliubDenisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabPredaSlavoliubDenisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/splab_predaslavoliubdenis/SpLabPredaSlavoliubDenisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splab_predaslavoliubdenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLabPredaSlavoliubDenisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
